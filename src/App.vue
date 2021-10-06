7e0a445a985e47c9221e9887802a2159ca375d60
Sergio9815 <gsergio1598@gmail.com>
2021-10-06 03:06:03 -0500

    :art: update main styles

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <div id="app">
     <lpHeader/>
+    <lp-main/>
   </div>
 </template>
 
 <script>
 import lpHeader from './components/lpHeader.vue'
+import lpMain from './components/lpMain.vue'
 
 export default {
   name: 'App',
   components: {
-    lpHeader
+    lpHeader,
+    lpMain
   }
 }
 </script>
 
 <style>
+
+html {
+  scroll-behavior: smooth;
+}
+
+/* --- --- -- COLORS -- --- --- */
+:root {
+  --dark: #161616;
+  --darkLg: #1616163b;
+  --white: #ffff;
+  --brown: #7D5A50;
+  --brownLg: #7d5a50a8;
+  --grey: #e6e6e6;
+  --red: #FD6F96;
+  --blue: #7fd4ff;
+  --green: #71EFA3;
+}
 
 body {
   margin: 0;
@@ -30,4 +50,5 @@ body {
   text-align: center;
   color: #2c3e50;
 }
+
 </style>
